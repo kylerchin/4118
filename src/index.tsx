5918ec8745c3c61ac6bf496417c755b6d4d1a897
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-06-25 00:23:04 -0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,6 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import './index.css';
 import App from './App';
 
-import TagManager from 'react-gtm-module'
- 
-const tagManagerArgs = {
-  gtmId: 'G-MRSNR9Z25J'
-}
-
-TagManager.initialize(tagManagerArgs)
-
-
 ReactDOM.render(
 <React.StrictMode>
 <App />
